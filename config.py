99f83f9cb90494c92bd9a01153c051288c5605e5
Shauna Gordon-McKeon <shaunagm@gmail.com>
2023-04-28 15:04:21 -0400

    Whoops add missing slash on new domain name

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ local_development = os.environ.get("LOCAL_DEVELOPMENT", False)
 if local_development:
     serverpath = "http://0.0.0.0:8000/"  # the path the site is served on
 else:
-    serverpath = "https://governingopen.com"
+    serverpath = "https://governingopen.com/"
